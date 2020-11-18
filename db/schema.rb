05f8ac60601b31e8f34aed135659814213bf7d68
seika-tech <mifune1227@gmail.com>
2020-11-18 20:43:22 +0900

    新規登録機能を実装する

--- db/schema.rb
+++ db/schema.rb
@@ -13,10 +13,16 @@
 ActiveRecord::Schema.define(version: 2020_11_04_072230) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "nickname", default: "", null: false
+    t.string "last_name", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "last_name_kana", default: "", null: false
+    t.string "first_name_kana", default: "", null: false
+    t.date "birth_date", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
+    t.time "rdateeset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
